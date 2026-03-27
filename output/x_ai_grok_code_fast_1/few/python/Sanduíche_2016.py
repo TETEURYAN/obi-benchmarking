897c6f07e08a87d0a1a11edea27cd167b1eb8e3c
mestrevh <victor-torugo@hotmail.com>
2026-03-27 16:45:17 -0300

    result: added the resul model grok code fast 1

--- output/x_ai_grok_code_fast_1/few/python/Sanduíche_2016.py
+++ output/x_ai_grok_code_fast_1/few/python/Sanduíche_2016.py
@@ -2,35 +2,60 @@ import sys
 from collections import defaultdict
 
 input_data = sys.stdin.read().split()
-N = int(input_data[0])
-D = int(input_data[1])
-C = list(map(int, input_data[2:]))
+idx = 0
+N = int(input_data[idx])
+idx += 1
+D = int(input_data[idx])
+idx += 1
+C = [int(input_data[idx + i]) for i in range(N)]
 
-prefix = [0] * (N + 1)
-for i in range(1, N + 1):
-    prefix[i] = prefix[i - 1] + C[i - 1]
+prefix = [0]
+for c in C:
+    prefix.append(prefix[-1] + c)
+total = prefix[-1]
 
-S = prefix[N]
-count = 0
-
-# subsequências contínuas
-freq = defaultdict(int)
-freq[0] = 1
+# Continuous subarrays
+count1 = 0
+counter = defaultdict(int)
+counter[0] = 1
 for j in range(1, N + 1):
     target = prefix[j] - D
-    if target in freq:
-        count += freq[target]
-    freq[prefix[j]] += 1
+    if target in counter:
+        count1 += counter[target]
+    counter[prefix[j]] += 1
 
-# extremidades
-if N >= 2:
-    freq2 = defaultdict(int)
-    target_diff = D - S
-    for i in range(1, N):
-        freq2[prefix[i]] += 1
-        j = i + 1
-        target = prefix[i] + target_diff
-        if target in freq2:
-            count += freq2[target]
+# Extremities
+diff = D - total
+all_prefix = prefix[:-1]  # 0 to N-1
+unique = sorted(set(all_prefix))
+rank_map = {val: i + 1 for i, val in enumerate(unique)}
+M = len(unique)
 
-print(count)
+class Fenwick:
+    def __init__(self, size):
+        self.size = size
+        self.tree = [0] * (size + 1)
+
+    def update(self, index, delta):
+        while index <= self.size:
+            self.tree[index] += delta
+            index += index & -index
+
+    def query(self, index):
+        sum_val = 0
+        while index > 0:
+            sum_val += self.tree[index]
+            index -= index & -index
+        return sum_val
+
+fenwick = Fenwick(M)
+count2 = 0
+for i in range(N - 1, 0, -1):
+    target = prefix[i] - diff
+    if target in rank_map:
+        rank = rank_map[target]
+        count2 += fenwick.query(rank)
+    rank_i = rank_map[prefix[i]]
+    fenwick.update(rank_i, 1)
+
+print(count1 + count2)
